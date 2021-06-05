f403ed20bbf918bda23214a84a7c4e6c553ea79d
seotrot <usmaniseo@gmail.com>
2021-06-05 17:17:34 +0500

    new code added

--- index.js
+++ index.js
@@ -1,90 +1,15 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+// randome message generator
 
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
+const rmg = () => {
+  const array = [
+    'Build a message generator program using JavaScript',
+    'Use Git version control',
+    'Use command line',
+    'Develop locally on your computer',
+  ]
 
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4]
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3]
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
-
-// An array of all the arrays above
-const batch = [
-  valid1,
-  valid2,
-  valid3,
-  valid4,
-  valid5,
-  invalid1,
-  invalid2,
-  invalid3,
-  invalid4,
-  invalid5,
-  mystery1,
-  mystery2,
-  mystery3,
-  mystery4,
-  mystery5,
-]
-
-const CreditValidator = (arr) => {
-  let double = arr.map((a, i) => {
-    return i % 2 == 0 ? a * 2 : a
-  })
-  let sub = double.map((a) => {
-    return a > 9 ? a - 9 : a
-  })
-  let sum = sub.reduce((a, c) => a + c, 0)
-  if (sum % 10 === 0) {
-    return true
-  }
-  return false
-}
-console.log(CreditValidator(valid3))
-
-const findInvalidCard = (nestedArray) => {
-  let invalidCards = nestedArray.map((a) => {
-    if (CreditValidator(a) === false) return a
-  })
-  let CleanArray = invalidCards.filter((i) => i !== undefined)
-  return CleanArray
+  const randomeNumber = Math.floor(Math.random() * 5)
+  return array[randomeNumber]
 }
 
-// console.log(findInvalidCard(batch))
-
-const idInvalidCardCompanies = (nesArr) => {
-  let findindex = findInvalidCard(
-    nesArr.map((i) => {
-      return i.filter((a, i) => {
-        return i === 0 ? a : undefined
-      })
-    })
-  )
-  let Companies = findindex.map((i) => {
-    return i[0] === 3
-      ? 'Amex'
-      : i[0] === 4
-      ? 'Visa'
-      : i[0] === 5
-      ? 'Master Card'
-      : i[0] === 4
-      ? 'Discover'
-      : 'Company not found'
-  })
-  return [...new Set(Companies)]
-}
-
-console.log(idInvalidCardCompanies([invalid2]))
-
-// second change the made
+console.log(rmg())
